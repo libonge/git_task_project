3ae84edeb57e5b0b0be3e3dfd3b4a39d610d7439
iamlibonge <libongembini22@gmail.com>
2024-01-25 21:49:29 +0200

    add comments to code

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,8 @@
-print('Hello World!')
+#Requests the the user to enter their name
+name = input("what is your name ? ")
+
+#Requests  the user to enter their thoughts
+thoughts = input("what are your thinking of ? ")
+
+#Print the user's name and thoughts
+print(f"{name} is thinking of {thoughts} ")
