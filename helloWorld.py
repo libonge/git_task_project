a113d1d5acc815b2a462af891239dcec7c2746b2
iamlibonge <libongembini22@gmail.com>
2024-01-30 13:22:59 +0200

    implented age for issue #1

--- helloWorld.py
+++ helloWorld.py
@@ -1,8 +1,9 @@
 #Requests the the user to enter their name
 name = input("what is your name ? ")
+age = input("enter age ")
 
 #Requests  the user to enter their thoughts
 thoughts = input("what are your thinking of ? ")
 
 #Print the user's name and thoughts
-print(f"{name} is thinking of {thoughts} ")
+print(f"{name} is thinking of {thoughts} and is {age} years old.")
