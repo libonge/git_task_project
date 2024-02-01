b2b6be0df3a6f6b8015e97a06138dd23e8b6ec7d
iamlibonge <libongembini22@gmail.com>
2024-02-01 16:48:48 +0200

    There age now is validated by program

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,14 @@
-name = input("What is your name ? : ")
+## Requests the user to enter their name
+name = input("What is your name ? ")
+
+# Validates the age input
+while True:
+    age = input("Enter your age : ")
+    if age.isdigit() and 0 <= int(age) <= 150:
+        break
+    else:
+        print("Invalid age! , Please enter a numerical value between 0 and 150.")
 
 user_input = input(" Say something : ")
  
-print (f'{name} said that " {user_input} " ')
+print (f'{name} said that " {user_input}. " ')
